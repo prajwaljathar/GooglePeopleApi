3632c3bf35b3e9942b31dc09f1126ff1e00227f4
PrajwalJathar <62589799+prajwaljathar@users.noreply.github.com>
2023-03-14 23:45:21 +0530

    Add files via upload

---
+++ googlepeopleapi/src/main/java/com/example/googlepeopleapi/GooglepeopleapiApplication.java
@@ -0,0 +1,15 @@
+package com.example.googlepeopleapi;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglepeopleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglepeopleapiApplication.class, args);
+	}
+
+}
---
+++ googlepeopleapi/src/test/java/com/example/googlepeopleapi/GooglepeopleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlepeopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglepeopleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
